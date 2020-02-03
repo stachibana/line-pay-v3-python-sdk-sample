9c2a96a916f27a282bc049a96e8e455f59e957ce
TACHIBANA SHO <JP21988@w010D71704054m-7043.local>
2020-02-03 19:12:10 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Overview
+
+LINE Pay API v3のPython版SDKの動作を確認出来るWeb Appです。[kintone](https://kintone.cybozu.co.jp/)と連携しているため、過去の取引の一覧を確認したり、返金、オーソリ無効化、自動決済(サブスク)の動作確認も可能です。
+
+[SDK](https://github.com/sumihiro3/line-pay-sdk-python)
+
+# Prepare
+
+## LINE Pay API
+
+* `.env_sample`を`.env`にリネーム
+* `.env`記載のID、SECRETをご自身のものに置き換え
+  * 加盟店審査が完了している方はご自身のもので
+  * まだの方は[Sandbox](https://pay.line.me/jp/developers/techsupport/sandbox/testflow?locale=ja_JP)を作成
+* Sandboxを利用しない方は`app.py`の上の方の`LINE_PAY_IS_SANDBOX`を`False`に変更
+
+## kintone
+
+* [開発者ライセンス](https://developer.cybozu.io/hc/ja/articles/360025742471)の申し込み
+* `account.yml_sample`を`account.yml`にリネーム
+* 以下の画像の通り設定
+
+![kintone description](https://cl.ly/28f3d463696e/download/kintone_description.png)
+
+## Execute
+
+```bash
+$ pip install -r requirements.txt
+$ python app.py
+```
+
+__Then needs to use ngrok or some tunneling tool to check!__
---
+++ app.py
@@ -0,0 +1,325 @@
+from flask import Flask, request, render_template, abort, redirect, jsonify
+from flask_bootstrap import Bootstrap
+import uuid
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+from linepay import LinePayApi
+import pykintone
+from pykintone import model
+import datetime
+
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+# dotenv
+load_dotenv(verbose=True)
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+# LINE Pay API
+LINE_PAY_CHANNEL_ID = os.environ.get("LINE_PAY_CHANNEL_ID")
+LINE_PAY_CHANNEL_SECRET = os.environ.get("LINE_PAY_CHANNEL_SECRET")
+LINE_PAY_IS_SANDBOX = True  # True | False
+api = LinePayApi(LINE_PAY_CHANNEL_ID,
+                 LINE_PAY_CHANNEL_SECRET, is_sandbox=LINE_PAY_IS_SANDBOX)
+
+# Cache
+CACHE = {}
+
+# kintone
+account = pykintone.load("account.yml")
+kintone = account.app()
+
+
+class PayTransaction(model.kintoneModel):
+    def __init__(self):
+        super(PayTransaction, self).__init__()
+        self.transaction_id = ""
+        self.transaction_type = ""
+        self.method = ""
+        self.total_amount = 0
+        self.currency = ""
+        self.payment_status = ""
+        self.reg_key = ""
+
+
+def get_environment():
+    return LINE_PAY_IS_SANDBOX
+
+
+app.jinja_env.globals.update(get_environment=get_environment)
+
+
+@app.route('/')
+def do_get():
+    pay_transactions = kintone.select().models(PayTransaction)
+    return render_template('index.html', payTransactions=pay_transactions)
+
+
+@app.route('/cancel')
+def cancel():
+    return redirect('/')
+
+
+@app.route('/request/<param_capture>')
+def reserve_payment(param_capture):
+    order_id = str(uuid.uuid4())
+    amount = 1
+    currency = "JPY"
+    CACHE["order_id"] = order_id
+    CACHE["amount"] = amount
+    CACHE["currency"] = currency
+    request_options = {
+        "amount": amount,
+        "currency": currency,
+        "orderId": order_id,
+        "packages": [
+            {
+                "id": "package-999",
+                "amount": 1,
+                "name": "Sample package",
+                "products": [
+                        {
+                            "id": "product-001",
+                            "name": "Sample product",
+                            "imageUrl": "https://placehold.jp/99ccff/003366/150x150.png?text=Sample%20product",
+                                        "quantity": 1,
+                                        "price": 1
+                        }
+                ]
+            }
+        ],
+        "options": {
+            "payment": {
+                "capture": True if param_capture == 'capture' else False
+            }
+        },
+        "redirectUrls": {
+            "confirmUrl": request.host_url.replace('http', 'https') + "confirm",
+            "cancelUrl": request.host_url.replace('http', 'https') + "cancel"
+        }
+    }
+    response = api.request(request_options)
+
+    return redirect(response['info']['paymentUrl']['web'])
+
+
+@app.route('/checkout/')
+def checkout():
+    order_id = str(uuid.uuid4())
+    amount = 1
+    currency = "JPY"
+    CACHE["order_id"] = order_id
+    CACHE["amount"] = amount
+    CACHE["currency"] = currency
+    request_options = {
+        "amount": amount,
+        "currency": currency,
+        "orderId": order_id,
+        "packages": [
+            {
+                "id": "package-999",
+                "amount": 1,
+                "name": "Sample package",
+                "products": [
+                        {
+                            "id": "product-001",
+                            "name": "Sample product",
+                            "imageUrl": "https://placehold.jp/99ccff/003366/150x150.png?text=Sample%20product",
+                                        "quantity": 1,
+                                        "price": 1
+                        }
+                ]
+            }
+        ],
+        "options": {
+            "payment": {
+                "capture": True
+            },
+            "shipping": {
+                "type": "SHIPPING",
+                "feeInquiryUrl": request.host_url.replace('http', 'https') + "v1/shippings/methods/get/"
+            }
+        },
+        "redirectUrls": {
+            "confirmUrl": request.host_url.replace('http', 'https') + "confirm",
+            "cancelUrl": request.host_url.replace('http', 'https') + "cancel"
+        }
+    }
+    response = api.request(request_options)
+
+    return redirect(response['info']['paymentUrl']['web'])
+
+
+@app.route('/v1/shippings/methods/get/', methods=["POST"])
+def inquiryShippingMethods():
+    result = {
+        "returnCode": "0000",
+        "returnMessage": "Example",
+        "info": {
+            "shippingMethods": [
+                {
+                    "id": "hoge",
+                    "name": "hogehoge express",
+                    "amount": "1000",
+                    "toDeliveryYmd": datetime.datetime.now().strftime("%Y%m%d")
+                }
+            ]
+        }
+    }
+    return jsonify(result)
+
+
+@app.route('/pay_get_regkey/')
+def pay_get_regkey():
+    order_id = str(uuid.uuid4())
+    amount = 1
+    currency = "JPY"
+    CACHE["order_id"] = order_id
+    CACHE["amount"] = amount
+    CACHE["currency"] = currency
+    request_options = {
+        "amount": amount,
+        "currency": currency,
+        "orderId": order_id,
+        "packages": [
+            {
+                "id": "package-999",
+                "amount": 1,
+                "name": "Sample package",
+                "products": [
+                        {
+                            "id": "product-001",
+                            "name": "Sample product",
+                            "imageUrl": "https://placehold.jp/99ccff/003366/150x150.png?text=Sample%20product",
+                                        "quantity": 1,
+                                        "price": 1
+                        }
+                ]
+            }
+        ],
+        "options": {
+            "payment": {
+                "capture": True,
+                "payType": "PREAPPROVED"
+            }
+        },
+        "redirectUrls": {
+            "confirmUrl": request.host_url.replace('http', 'https') + "confirm",
+            "cancelUrl": request.host_url.replace('http', 'https') + "cancel"
+        }
+    }
+    response = api.request(request_options)
+
+    return redirect(response['info']['paymentUrl']['web'])
+
+
+@app.route('/capture/<int:transaction_id>/<float:amount>/<currency>/')
+def capture_transaction(transaction_id, amount, currency):
+    response = api.capture(transaction_id, amount, currency)
+
+    details = api.payment_details(transaction_id)
+
+    pay_transaction = kintone.select(
+        "transaction_id=" + str(transaction_id)).models(PayTransaction)[0]
+    pay_transaction.payment_status = details['info'][0]['payStatus']
+
+    kintone.update(pay_transaction)
+
+    return redirect('/')
+
+
+@app.route('/void/<int:transaction_id>/')
+def void_authorization(transaction_id):
+    response = api.void(transaction_id)
+
+    details = api.payment_details(transaction_id)
+
+    pay_transaction = kintone.select(
+        "transaction_id=" + str(transaction_id)).models(PayTransaction)[0]
+    pay_transaction.payment_status = details['info'][0]['payStatus']
+
+    kintone.update(pay_transaction)
+
+    return redirect('/')
+
+
+@app.route('/refund/<int:transaction_id>/<float:refund_amount>/')
+def refund(transaction_id, refund_amount):
+    response = api.refund(transaction_id)
+
+    transaction = PayTransaction()
+    transaction.transaction_id = response['info']['refundTransactionId']
+    transaction.transaction_type = 'REFUND'
+    transaction.method = 'N/A'
+    transaction.total_amount = refund_amount
+    transaction.currency = 'N/A'
+    transaction.payment_status = 'N/A'
+    transaction.reg_key = ''
+    kintone.create(transaction)
+
+    pay_transaction = kintone.select(
+        "transaction_id=" + str(transaction_id)).models(PayTransaction)[0]
+    pay_transaction.payment_status = 'REFUNDED'
+
+    kintone.update(pay_transaction)
+
+    return redirect('/')
+
+
+@app.route("/confirm")
+def pay_confirm():
+    transaction_id = int(request.args.get('transactionId'))
+
+    CACHE["transaction_id"] = int(transaction_id)
+    response = api.confirm(
+        transaction_id,
+        float(CACHE.get("amount", 0)),
+        CACHE.get("currency", "JPY")
+    )
+
+    details = api.payment_details(transaction_id)
+
+    transaction = PayTransaction()
+    transaction.transaction_id = transaction_id
+    transaction.transaction_type = details['info'][0]['transactionType']
+    transaction.method = details['info'][0]['payInfo'][0]['method']
+    transaction.total_amount = details['info'][0]['payInfo'][0]['amount']
+    transaction.currency = details['info'][0]['currency']
+    transaction.payment_status = details['info'][0]['payStatus']
+    if 'regKey' in response['info']:
+        transaction.reg_key = response['info']['regKey']
+    kintone.create(transaction)
+
+    return redirect('/')
+
+
+@app.route("/pay_preapproved/<reg_key>/")
+def pay_preapproved(reg_key):
+
+    order_id = str(uuid.uuid4())
+    amount = 1
+    currency = "JPY"
+    product_name = "定期購入アイテム"
+
+    response = api.pay_preapproved(
+        reg_key, product_name, float(amount), currency, order_id, True)
+
+    details = api.payment_details(response['info']['transactionId'])
+
+    transaction = PayTransaction()
+    transaction.transaction_id = response['info']['transactionId']
+    transaction.transaction_type = "PAYMENT(PREAPPROVED)"
+    transaction.method = "Preapproved method"
+    transaction.total_amount = amount
+    transaction.currency = currency
+    transaction.payment_status = details['info'][0]['payStatus']
+    kintone.create(transaction)
+
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
